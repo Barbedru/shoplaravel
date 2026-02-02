94af4afa943745634b91785554c9c2bd35427649
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-02-02 14:53:25 +0100

    Créer un controller - Route avec paramètre - Nommer les routes

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return redirect()->route('products.show', ['id' => 42]) ;
+    }
+
+    public function about()
+    {
+        return 'À propos de nous';
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello', function () {
     return 'Hello Laravel!';
 });
+
+
+use App\Http\Controllers\PageController;
+
+Route::get('/home', [PageController::class, 'home'])
+    ->name('home');
+    
+Route::get('/about', [PageController::class, 'about'])
+    ->name('about');
+    
+
+
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products/{id}', [ProductController::class, 'show'])
+    ->name('products.show');
